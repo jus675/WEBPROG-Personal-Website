32500a85fd6e8ca51675f91285246e05938e2ff6
Justine Denise Hernandez <114111385+jus675@users.noreply.github.com>
2024-12-16 14:58:26 +0800

    edited javascript function

--- home/js/myscript.js
+++ home/js/myscript.js
@@ -1,3 +1,8 @@
 function myFunction() {
-    document.getElementById("demo").innerHTML = "Paragraph changed.";
+    document.getElementById("demo").innerHTML = "Thank you <3";
   }
+
+function myFunction2() {
+  const element = document.getElementsByTagName("p");
+  document.getElementById("demo").innerHTML = "Pop! &#129767 &#129767 &#129767 &#129767 &#129767 &#129767";
+}
